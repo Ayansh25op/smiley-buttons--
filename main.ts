1a27a6ce035919d2d6cf3c91d1a0f22785d24e44
Ayansh25op <87590978+Ayansh25op@users.noreply.github.com>
2021-10-04 10:35:42 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
+    while (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     for (let index = 0; index < 4; index++) {
@@ -40,11 +42,13 @@ input.onButtonPressed(Button.AB, function () {
     }
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . . . .
-        . # # # .
-        # . . . #
-        `)
+    while (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+    }
 })
